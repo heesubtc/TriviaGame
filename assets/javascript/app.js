d315c16dd6743259fe32acdd96d0e2f383b53081
reddbacchus <reddbacchus@gmail.com>
2018-10-16 00:19:55 -0400

    initial site files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,5 @@
-// console.log("helloworld");
-
-// #start-button;
-// #submit-button;
-
-
-// // clock function
-// function clock(
-
-
-// function check() {
-
-// }
-
-
-
-// set variable for correct counter
-// set variable for incorrect counter
-// set variables for correct answers for all 5 questions using jquery val()
+// console.log("hello");
+// VARS
 var correct = 0;
 var incorrect = 0;
 var question1 = $("input.question1").val();
@@ -24,33 +7,39 @@ var question2 = $("input.question2").val();
 var question3 = $("input.question3").val();
 var question4 = $("input.question4").val();
 var question5 = $("input.question5").val();
-
 var clock = $("p#clock")
 var timer = 0;
-var timeleft = 20;
+var timeleft = 7;
 
+// PLAY GAME BUTTON THAT CALLS CLOCK COUNTDOWN FUNCTION
+$("#start-button").on("click", function() {
+    $("h1#jumbo").text("Totally Trivial Trivia!");
+    countDown();
+    $("h3#number-correct").empty();       
+    $("h3#number-incorrect").empty();
+    correct = 0;
+    incorrect = 0;
+    $("h3#giphy").css("visibility", "hidden");
+    $("input[type='radio']").prop("checked", false);
+});
+
+// FUNCTION TO COUNT DOWN CLOCK FROM 10 SECS
 function countDown() {
     clock.text(timeleft - timer);
     function runTimer() {
         timer++;
         clock.text(timeleft - timer);
+        if (timer === timeleft) {
+            clearInterval(interval); 
+            $("h1#jumbo").empty();
+            $("h1#jumbo").text("Times Up!");
+            timeleft += 7;       
+        }
     }
-    setInterval(runTimer, 1000);
+    var interval = setInterval(runTimer, 1000);
 }
 
-
-
-
-
-
-$("#start-button").on("click", function() {
-    countDown();
-});
-
-
-
-
-//function to verify if the answers are correct
+// FUNCTION TO CHECK IF ANSWERS ARE CORRECT AND KEEP TALLY
 function verify() {
     if ($("input[name=question1]:checked").val() === "3") {
         correct ++;
@@ -76,78 +65,23 @@ function verify() {
         correct ++;
     } else if (question1 !== "1st") {
         incorrect++;
+    }    
+}
+
+// DISPLAYS PICTURE IF PERFECT SCORE
+function display() {
+    if (incorrect === 0) {
+    $("h3#giphy").css("visibility", "visible")
     }
 }
-// function verifyq2() {
-//     if ($("input[name=question2]:checked").val() === "THE ANSWER") {
-//         correct ++;
-//     } else if(question1 !== "THE ANSWER") {
-//         incorrect++;
-//     }
-// }
 
-    // if (question2 === "THE ANSWER") {
-    //     correct++;
-    // } else if(question2 !== "THE ANSWER") {
-    //     incorrect++;
-    // }
-    // if (question3 === 8) {
-    //     correct++;
-    // } else if(question3 !== 8){
-    //     incorrect++;
-    // }
-    // if (question4 === "All of the above") {
-    //     correct++;
-    // } else if(question4 !== "All of the above") {
-    //     incorrect++;
-    // }
-    // if (question5 === "1st") {
-    //     correct++;
-    // } else if(question5 !== "1st"){
-    //     incorrect++;
-    // }
-
-
-// function verifyq1() {
-//     if (question1 === "3") {
-//         correct++;
-//     } else if (question1 !== "3") {
-//         incorrect++;
-//     }
-//     console.log(correct);
-// }
-
-
-
-
-//when submit-button is clicked
-////call function to verify answers and tally count
-////submitted div counter displays to visible 
-////total number correct to be displayed in html
-////total number incorrect to be dislayed in html
-$("button#submit-button").on("click", function() {
-    verify();
-    $("p#number-correct").text(correct + " Correct");
-    $("p#number-incorrect").text(incorrect + " Incorrect");
-    $("#submitted").css("visibility", "visible");
+// SUBMIT BUTTON CALL VERIFY FUNCTION AND DISPLAY SCORE
+$("button#submit-button").on("click", function() {          
+    verify();                                               
+    $("h3#number-correct").text(correct + " Correct");       
+    $("h3#number-incorrect").text(incorrect + " Incorrect"); 
+    $("#submitted").css("visibility", "visible");             
+    display();
 })
 
-// $("#submit-button").on("click", function() {
-//     verify();
-// }
 
-
-// var message = ["Well done!", "Not bad", "You're better than that!"]
-// var numberofquestionscorrect;
-//     if (correct < 1) {
-//         numberofquestionscorrect = 2;
-//     }
-//     if (correct < 5) {
-//         numberofquestionscorrect = 1;
-//     }
-//     if (correct > 4) {
-//         numberofquestionscorrect = 0;
-//     }
-
-
-// $("#number-correct").html(message[numberofquestionscorrect]);
